df588e5fdb7c22b336b6092cff9cebeda9d19996
anamika-malakar-perp <64518698+anamika-malakar-perp@users.noreply.github.com>
2023-03-01 17:31:02 +0530

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,5 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
