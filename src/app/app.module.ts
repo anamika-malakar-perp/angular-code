36842b65878c15e414f53d9786ef884c4aca3aa2
anamika-malakar-perp <64518698+anamika-malakar-perp@users.noreply.github.com>
2023-03-03 15:22:55 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppHighlight } from './directives/basic-highlight.directive';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, AppHighlight],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
