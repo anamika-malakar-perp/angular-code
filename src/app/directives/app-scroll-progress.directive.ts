254ac216fd5e7ac329ca20b4c07f7791d5ec0ca5
anamika-malakar-perp <64518698+anamika-malakar-perp@users.noreply.github.com>
2023-03-06 16:02:02 +0530

    3 files modified

--- src/app/directives/app-scroll-progress.directive.ts
+++ src/app/directives/app-scroll-progress.directive.ts
@@ -7,8 +7,20 @@ export class AppScrollProgress {
   constructor() {
     console.log('call');
   }
-  @HostListener('scroll', ['$event'])
-  doSomething(event) {
-    console.log('testing', event);
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    var h = document.documentElement;
+    console.log('h', h);
+    // var b = document.body;
+    // console.log('b', b);
+    var scrollTop = 'scrollTop';
+    var scrollHeight = 'scrollHeight';
+    console.log('h-data',parseInt(h[scrollTop]))
+    // console.log('b-top',b[scrollTop])
+    console.log('h-height',h[scrollHeight])
+    // console.log('b-height',b[scrollHeight])
+    console.log('client-height',h.clientHeight)
+    // b = document.body,
+    // this.progresValue = (h[scrollTop]||b[scrollTop]) / ((h[scrollHeight]||b[scrollHeight]) - h.clientHeight) * 100;
   }
 }
