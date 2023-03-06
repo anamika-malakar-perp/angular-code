a1db944fc9fd43f4ab880bec3e57b5bf975b8c68
anamika-malakar-perp <64518698+anamika-malakar-perp@users.noreply.github.com>
2023-03-06 14:30:58 +0530

    2 files modified, 1 deleted

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,23 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthComponent } from './auth.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {
-    path: '',
-    component: AuthComponent,
-    children: [
-      {
-        path: 'login',
-        component: LoginComponent
-      }
-    ]
+    path: 'login',
+    component: LoginComponent,
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
 export class AuthRoutingModule {}
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AuthRoutingModule } from './auth-routing.module';
-import { AuthComponent } from './auth.component';
 import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   imports: [CommonModule, AuthRoutingModule],
-  declarations: [AuthComponent, LoginComponent],
+  declarations: [LoginComponent],
   providers: [],
 })
 export class AuthModule {}
