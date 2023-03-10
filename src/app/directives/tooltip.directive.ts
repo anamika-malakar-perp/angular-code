4590126565662339c9712143d71d3d9989ccccf0
anamika-malakar-perp <64518698+anamika-malakar-perp@users.noreply.github.com>
2023-03-10 12:40:36 +0530

    1 file modified

--- src/app/directives/tooltip.directive.ts
+++ src/app/directives/tooltip.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener, Input } from "@angular/core";
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from "@angular/core";
 
 @Directive({
   selector: '[toolTip]'
@@ -8,13 +8,18 @@ export class ToolTip {
   @Input()
   toolTip: string;
 
-  constructor(private element: ElementRef) {
+  hoverTooltip
+
+  // The role of ElementRef is to provide us with direct access to the DOM element, which will be available through a property called nativeElement.
+
+  //The Renderer2 allows us to manipulate the DOM elements, without accessing the DOM directly. It provides a layer of abstraction between the DOM element and the component code. Using Renderer2 we can create an element, add a text node to it, append child element using the appendchild method., etc. We can also add or remove styles, HTML attributes, CSS Classes & properties, etc. We can also attach and listen to events etc.
+  constructor(private element: ElementRef, private render: Renderer2) {
 
   }
 
   @HostListener('mouseenter')
   onMouseEnter() {
-    
+    console.log(!this.hoverTooltip)
   }
 
   @HostListener('mouseleave')
