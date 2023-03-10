b10d9dd1b2163e609653119e2b6764c0074d95b2
anamika-malakar-perp <64518698+anamika-malakar-perp@users.noreply.github.com>
2023-03-10 15:50:48 +0530

    1 file modified, 1 added

---
+++ src/app/modules/auth/components/practice-question.ts
@@ -0,0 +1,56 @@
+// //sum(3,5)
+// //sum(3)(5)
+
+// function sum(a,b) {
+//   if(!b){
+//   return function sum(b) {
+//       return a + b
+//   }
+// }
+//   return a+b
+// }
+
+// console.log(sum(3)(5))
+// console.log(sum(3,5))
+
+// let a = [1,2,3,6,5,0]
+// // let largest = a[0];
+// // let arr  =[]
+// // for(let i =0;i< a.length;i++) {
+// //     if(a[i] > largest) {
+// //         largest = a[i]
+// //     }
+// // }
+// let count=0;
+// let total=0;
+
+// for(let i = 0 ;i<a.length;i++){
+//   // if(!a.includes(i)){
+//   //     console.log(i)
+//   //     break;
+//   // }
+//   count = count+a[i]
+//   total = total+i
+// }
+// total = total+a.length
+
+// console.log(total-count)
+
+// arr = [1, 2, 3, [4, 5 ,6, [7, 8, 9]]]
+// let j = []
+// for(let i of arr){
+//   if(typeof(i)=='number'){
+//       j.push(i)
+//   }else if(Array.isArray(i)){
+//       j = [...j,...i]
+//   }
+// }
+
+// console.log(j)
+
+
+// // for(let i = 0;i< a.length;i++) {
+// //     if(a[i] > largest )
+// // }
+// //console.log(a.sort((a,b) => a - b))
+
