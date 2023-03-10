59a81e2d96a1cf54485d0bc3573658a8cdc8279f
anamika-malakar-perp <64518698+anamika-malakar-perp@users.noreply.github.com>
2023-03-10 10:21:41 +0530

    1 file modified

--- src/app/directives/hover-color.directive.ts
+++ src/app/directives/hover-color.directive.ts
@@ -1,17 +1,18 @@
-import { Directive, ElementRef, HostListener } from "@angular/core";
+import { Directive, ElementRef, HostListener, Renderer2 } from "@angular/core";
 
 @Directive({
   selector: '[hoverColor]'
 })
 
 export class OnHoverColor {
-  constructor(private element : ElementRef) {
+  constructor(private element : ElementRef, private render: Renderer2) {
 
   }
 
   // the Hostlistener is a decorator that declares a DOM event to listen for and provides a handler method to run when that event occurs. 
   @HostListener('mouseenter')
   onMouseEnter() {
-    alert("You just hovered over a paragraph");
+    // alert("You just hovered over a paragraph");
+    this.render.setStyle(this.element.nativeElement, 'backgroundColor', 'red')
   }
 }
